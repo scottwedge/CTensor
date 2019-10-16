2b5172fc4ed9a1e19d4b43d91af4a56568126aab
annieyan <annieyan9033@hotmail.com>
2019-10-16 13:04:12 -0700

    fix minor

--- predictions/inference_autoencoder.py
+++ predictions/inference_autoencoder.py
@@ -677,7 +677,6 @@ def main():
     txt_name = save_path + 'autoencoder_v1_' +  timer + '.txt'
     with open(txt_name, 'w') as the_file:
         the_file.write('Only account for grids that intersect with city boundary \n')
-        the_file.write(str(beta) + '\n')
         the_file.write('place\n')
         the_file.write(str(place) + '\n')
         # the_file.write('use_1d_fea\n')
