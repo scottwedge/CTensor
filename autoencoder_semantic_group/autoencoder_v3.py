daae8d1e7780bd804e8b9ae02843eaaceacab198
annieyan <annieyan9033@hotmail.com>
2019-11-11 22:36:03 -0800

    fix minor

--- autoencoder_semantic_group/autoencoder_v3.py
+++ autoencoder_semantic_group/autoencoder_v3.py
@@ -641,7 +641,7 @@ class Autoencoder:
 
         # ------------------------------------------------#
         # dim: latent fea dimension
-        latent_fea = self.model_fusion(med_res_3d, med_res_2d, med_res_1d, dim, self.is_training)
+        latent_fea = self.fuse_and_train(list(second_level_output.values()),  self.is_training, dim)
         print('latent_fea.shape: ', latent_fea.shape) # (?, 32, 20, 3)
         # recontruction
         print('recontruction')
