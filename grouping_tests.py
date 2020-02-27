26cfd50b049fd2f2f657c7543f0ceb416dc9c5a7
annieyan <annieyan9033@hotmail.com>
2020-02-27 09:44:59 -0800

    traditional grouping, 1d2d = 0

--- grouping_tests.py
+++ grouping_tests.py
@@ -117,22 +117,19 @@ def first_level_grouping(feature_map_dict, encoded_list_rearrange_concat,
                     # then flatten and compare
                     # This means that there is no temporal variations for 2D
                     if ds_name2 in keys_2d:
-                        temp_arr2 = feature_map_dict[ds_name2][n,:,:,:] # 32, 20, 1
-                        # duplicate to [32, 20, 24]
-                        temp_arr2_mean_dup = np.repeat(temp_arr2, dim1, axis = -1)
-
-                        # compress 1D (32, 20, 3) duplicate to 32, 20, 1 by average,
-                        # temp_1d_mean = np.mean(temp_1d_dup, axis = -1)  #
-                        # temp_1d_mean = np.expand_dims(temp_1d_mean, axis = -1) # 32, 20, 1
-
-                        compress_arr2 = remove_outside_cells(temp_arr2_mean_dup, mask_arr) # [32, 20, 24]
-                        compress_arr1 = remove_outside_cells( temp_1d_dup, mask_arr) # [32, 20, 24]
-
-                        ave_SR = 0
-                        sim_sparse = cosine_similarity(compress_arr2.reshape(1, -1),
-                                                                   compress_arr1.reshape(1, -1))
-
-                        ave_SR = sim_sparse[0][0]
+                        # temp_arr2 = feature_map_dict[ds_name2][n,:,:,:] # 32, 20, 1
+                        # # duplicate to [32, 20, 24]
+                        # temp_arr2_mean_dup = np.repeat(temp_arr2, dim1, axis = -1)
+                        #
+                        # compress_arr2 = remove_outside_cells(temp_arr2_mean_dup, mask_arr) # [32, 20, 24]
+                        # compress_arr1 = remove_outside_cells( temp_1d_dup, mask_arr) # [32, 20, 24]
+                        #
+                        # ave_SR = 0
+                        # sim_sparse = cosine_similarity(compress_arr2.reshape(1, -1),
+                        #                                            compress_arr1.reshape(1, -1))
+                        #
+                        # ave_SR = sim_sparse[0][0]
+                        # relation_all_df.loc[ds_name1, ds_name2]  += ave_SR
                         relation_all_df.loc[ds_name1, ds_name2]  += ave_SR
 
                     # 3D VS 1D
@@ -696,10 +693,13 @@ def main():
         print('begin grouping')
         # relation_all_df = first_level_grouping_simplified(feature_map_dict, encoded_list_rearrange_concat,
         #             mask_arr, keys_list, keys_1d, keys_2d, keys_3d)
+        relation_all_df = first_level_grouping(feature_map_dict, encoded_list_rearrange_concat,
+                    mask_arr, keys_list, keys_1d, keys_2d, keys_3d)
 
 
-        relation_1d_df, relation_2d_df, relation_3d_df = first_level_grouping_within_group(feature_map_dict, encoded_list_rearrange_concat,
-             mask_arr, keys_list, keys_1d, keys_2d, keys_3d)
+
+        # relation_1d_df, relation_2d_df, relation_3d_df = first_level_grouping_within_group(feature_map_dict, encoded_list_rearrange_concat,
+        #      mask_arr, keys_list, keys_1d, keys_2d, keys_3d)
 
 
 
@@ -715,17 +715,17 @@ def main():
 
     # -------------------  relation_all_df -------------------------- #
 
-    # print('relation_all_df')
-    # print(relation_all_df)
-    # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
-    #
-    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    # clustering(relation_all_df, keys_list,txt_name,method, n_clusters)
-    #
-    # print('plotting')
-    # plot_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.png'
-    # plot_grouping(relation_all_df, plot_name)
-    # print('plot saved to :', plot_name)
+    print('relation_all_df')
+    print(relation_all_df)
+    relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
+
+    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    clustering(relation_all_df, keys_list,txt_name,method, n_clusters)
+
+    print('plotting')
+    plot_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.png'
+    plot_grouping(relation_all_df, plot_name)
+    print('plot saved to :', plot_name)
 
 
     # -------------------------------------------------------------#
@@ -734,24 +734,24 @@ def main():
     # ---------------------------   by dim ------------------------- #
 
 
-    print('relation_all_df')
-    print(relation_1d_df)
-    print(relation_2d_df)
-    print(relation_3d_df)
-    # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
-
-    print('relation_1d_df')
-    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    clustering(relation_1d_df, keys_1d,txt_name,method, n_clusters)
-
-
-    print('relation_2d_df')
-    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    clustering(relation_2d_df, keys_2d,txt_name,method, n_clusters)
-
-    print('relation_3d_df')
-    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    clustering(relation_3d_df, keys_3d,txt_name,method, n_clusters)
+    # print('relation_all_df')
+    # print(relation_1d_df)
+    # print(relation_2d_df)
+    # print(relation_3d_df)
+    # # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
+    #
+    # print('relation_1d_df')
+    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    # clustering(relation_1d_df, keys_1d,txt_name,method, n_clusters)
+    #
+    #
+    # print('relation_2d_df')
+    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    # clustering(relation_2d_df, keys_2d,txt_name,method, n_clusters)
+    #
+    # print('relation_3d_df')
+    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    # clustering(relation_3d_df, keys_3d,txt_name,method, n_clusters)
 
 
     # ----------------------------------------------------------- #
