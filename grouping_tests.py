27dbfcf4a75c2c35f56427e0620a8775b9534210
annieyan <annieyan9033@hotmail.com>
2020-02-28 21:12:42 -0800

    debug

--- grouping_tests.py
+++ grouping_tests.py
@@ -570,6 +570,7 @@ def second_level_grouping(feature_map_dict, encoded_list_rearrange_concat,
 
             for ds_name2 in all_keys:
                 temp_arr2 = feature_map_dict[ds_name2]
+                print('temp_arr2[n, :, :, :,:].shape',temp_arr2[n, :, :, :,:].shape, len(temp_arr2[n, :, :, :,:].shape))
                 compress_arr2 = remove_outside_cells( temp_arr2[n, :, :, :,:], mask_arr)
                 compress_arr1 = remove_outside_cells( temp_arr1[n, :, :, :,:], mask_arr)
 
