f9d83ae475d47457081189e20a875f9d0569c5ea
annieyan <annieyan9033@hotmail.com>
2020-03-23 22:25:36 -0700

    batchsize to 128

--- bike_count_prediction/lstm.py
+++ bike_count_prediction/lstm.py
@@ -34,11 +34,11 @@ TIMESTEPS = 168
 TRAINING_STEPS = 3000
 # if use data from 2017-10  - 2018- 03 as training data, there are 4000 data samples
 # that is 20 batches an epoch. Run 30 epoches, -> 600 steps
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 #PRINT_STEPS = TRAINING_STEPS / 100
 N_HIDDEN = 128 # previously 30
 LEARNING_RATE = 0.001
-CHANNEL = 1
+
 
 class generateData(object):
     def __init__(self, rawdata, timesteps, batchsize):
