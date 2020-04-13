0c7679507e83464fd36008b42ab3a4f31f69d69c
annieyan <annieyan9033@hotmail.com>
2020-04-13 11:13:13 -0700

    within dim grouping

--- grouping_tests.py
+++ grouping_tests.py
@@ -735,17 +735,17 @@ def main():
 
     # -------------------  relation_all_df -------------------------- #
 
-    print('relation_all_df')
-    print(relation_all_df)
-    relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
-
-    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    clustering(relation_all_df, keys_list,txt_name,method, n_clusters)
-
-    print('plotting')
-    plot_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.png'
-    plot_grouping(relation_all_df, plot_name)
-    print('plot saved to :', plot_name)
+    # print('relation_all_df')
+    # print(relation_all_df)
+    # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
+    #
+    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    # clustering(relation_all_df, keys_list,txt_name,method, n_clusters)
+    #
+    # print('plotting')
+    # plot_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.png'
+    # plot_grouping(relation_all_df, plot_name)
+    # print('plot saved to :', plot_name)
 
 
     # -------------------------------------------------------------#
@@ -754,24 +754,24 @@ def main():
     # ---------------------------   by dim ------------------------- #
 
 
-    # print('relation_all_df')
-    # print(relation_1d_df)
-    # print(relation_2d_df)
-    # print(relation_3d_df)
-    # # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
-    #
-    # print('relation_1d_df')
-    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    # clustering(relation_1d_df, keys_1d,txt_name,method, n_clusters)
-    #
-    #
-    # print('relation_2d_df')
-    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    # clustering(relation_2d_df, keys_2d,txt_name,method, n_clusters)
-    #
-    # print('relation_3d_df')
-    # txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
-    # clustering(relation_3d_df, keys_3d,txt_name,method, n_clusters)
+    print('relation_all_df')
+    print(relation_1d_df)
+    print(relation_2d_df)
+    print(relation_3d_df)
+    # relation_all_df.to_csv(encoding_dir+  level+  '_level'+ '_grouping_' + suffix + '.csv')
+
+    print('relation_1d_df')
+    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    clustering(relation_1d_df, keys_1d,txt_name,method, n_clusters)
+
+
+    print('relation_2d_df')
+    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    clustering(relation_2d_df, keys_2d,txt_name,method, n_clusters)
+
+    print('relation_3d_df')
+    txt_name = encoding_dir + '_'+ method+'_' +level+  '_level'+ '_grouping_' + suffix + '.txt'
+    clustering(relation_3d_df, keys_3d,txt_name,method, n_clusters)
 
 
     # ----------------------------------------------------------- #
