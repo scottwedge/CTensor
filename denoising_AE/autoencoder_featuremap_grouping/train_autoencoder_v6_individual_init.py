d26829fd0f0f25bc4f2b06613d8ee2aa3aec4142
annieyan <annieyan9033@hotmail.com>
2020-04-18 19:07:10 -0700

    denoise ae normalized 0.01, grouping within dim

--- denoising_AE/autoencoder_featuremap_grouping/train_autoencoder_v6_individual_init.py
+++ denoising_AE/autoencoder_featuremap_grouping/train_autoencoder_v6_individual_init.py
@@ -521,6 +521,8 @@ def main():
                 }
 
     '''
+
+    '''
     grouping_dict = {'one_dim_grp': ['precipitation','temperature', 'pressure', 'airquality'],
                     'two_dim_grp': ['house_price', 'POI_business','POI_food',  'POI_government',
                         'POI_hospitals', 'POI_publicservices', 'POI_recreation', 'POI_school',
@@ -528,7 +530,7 @@ def main():
                             'transit_signals', 'transit_stop', 'slope', 'bikelane'],
                      'third_dim_grp': ['building_permit', 'collisions', 'seattle911calls']
                     }
-
+    '''
 
     # ------ Raw data grouping V1 ------------- #
     '''
@@ -612,19 +614,18 @@ def main():
     # 'group_9':['seattle911calls'],
     # }
 
-    # updated on April 13, based on denoising_AE alltoall
+    # updated on April 17, based on denoising_AE alltoall
     # within dim, 1d2d = 0
-    # grouping_dict = {
-    # 'group_1': ['precipitation', 'temperature', 'pressure'],
-    # 'group_2': ['airquality'],
-    # 'group_3': ['house_price', 'POI_government', 'POI_hospitals'],
-    # 'group_4':['POI_business', 'POI_food', 'POI_publicservices', 'POI_recreation'],
-    # 'group_5': ['POI_school', 'seattle_street', 'total_flow_count', 'transit_routes',
-    #         'transit_signals', 'transit_stop', 'bikelane'],
-    # 'group_6': ['POI_transportation', 'slope'],
-    # 'group_7': ['building_permit'],
-    # 'group_8': ['collisions', 'seattle911calls'],
-    # }
+    grouping_dict = {
+    'group_1': ['precipitation', 'temperature', 'pressure'],
+    'group_2': ['airquality'],
+    'group_3': ['house_price'],
+    'group_4': ['POI_business', 'POI_school', 'total_flow_count', 'transit_stop'],
+    'group_5': ['POI_food', 'POI_government', 'POI_hospitals', 'POI_recreation', 'transit_routes', 'slope', 'bikelane'],
+    'group_6': ['POI_publicservices', 'POI_transportation', 'seattle_street', 'transit_signals'],
+    'group_7': ['building_permit', 'collisions'],
+    'group_8': ['seattle911calls'],
+    }
 
     # updated on April 15, based on denoising_AE alltoall
     # simplified auto grouping
