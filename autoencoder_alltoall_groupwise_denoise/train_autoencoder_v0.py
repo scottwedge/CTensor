f6bb1576606b2d614c0b7457e29effd0935730ba
annieyan <annieyan9033@hotmail.com>
2020-04-27 19:52:57 -0700

    fix

--- autoencoder_alltoall_groupwise_denoise/train_autoencoder_v0.py
+++ autoencoder_alltoall_groupwise_denoise/train_autoencoder_v0.py
@@ -648,6 +648,8 @@ def main():
         the_file.write(str(LEARNING_RATE) + '\n')
         the_file.write('key\n')
         the_file.write(str(key) + '\n')
+        the_file.write('TRAINING_STEPS\n')
+        the_file.write(str(TRAINING_STEPS) + '\n')
         the_file.write('selected_keys\n')
         for item in selected_keys:
             the_file.write("%s\n" % item)
