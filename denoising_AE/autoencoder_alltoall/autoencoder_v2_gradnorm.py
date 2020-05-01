d3bb4bc10fa4f7a075e641dd8cd7569cc76edf92
annieyan <annieyan9033@hotmail.com>
2020-04-30 22:56:35 -0700

    fix

--- denoising_AE/autoencoder_alltoall/autoencoder_v2_gradnorm.py
+++ denoising_AE/autoencoder_alltoall/autoencoder_v2_gradnorm.py
@@ -1041,7 +1041,7 @@ class Autoencoder:
                     else:  # if itr % 200 ! = 200, dont' use gradnorm
                         # sess.run(stardard_grad_lists, feed_dict= feed_dict_all)
                         batch_gradnorm_dict, _ = sess.run([gradnorm_dict, train_op], feed_dict= feed_dict_all)
-                        for k, v in batch_gradnorm_dict:
+                        for k, v in batch_gradnorm_dict.items():
                             print("Iter/Epoch: {}/{}...".format(itr, epoch), 'grad norm:{},{}:'.format(k, v))
 
 
