946874d8a1fb21a34f7d12c5e41c2c8a71d03c1d
annieyan <annieyan9033@hotmail.com>
2020-05-04 23:16:12 -0700

    change lr to 0.02 to ae v7

--- denoising_AE/autoencoder_featuremap_grouping_multilevel/train_autoencoder_v7.py
+++ denoising_AE/autoencoder_featuremap_grouping_multilevel/train_autoencoder_v7.py
@@ -320,7 +320,7 @@ def parse_args():
     parser.add_argument('-e',   '--epoch',  type=int,
                      action="store", help = 'epochs to train', default = 50)
     parser.add_argument('-l',   '--learning_rate',  type=float,
-                     action="store", help = 'epochs to train', default = 0.01)
+                     action="store", help = 'epochs to train', default = 0.02)
     # when do resume training, use_pretrained should be set to False
     parser.add_argument("-up","--use_pretrained", type=bool, default=True,
     				help="A boolean value whether or not to start from pretrained model")
