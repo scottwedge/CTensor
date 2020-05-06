c028dbd03e2b353fedc7a61a76f81c17403a6154
annieyan <annieyan9033@hotmail.com>
2020-05-05 22:29:26 -0700

    fix

--- denoising_AE/autoencoder_alltoall/autoencoder_v2_product.py
+++ denoising_AE/autoencoder_alltoall/autoencoder_v2_product.py
@@ -445,7 +445,7 @@ class Autoencoder:
     # (batchsize, 168, # of features)
     def cnn_1d_fuse(self, feature_map_list, is_training, suffix = '', output_dim =1, seed=None):
         # var_scope = "1d_data_process_" + suffix
-        output_dim = (len(feature_map_list) / 4) + 1
+        output_dim = int(len(feature_map_list) / 4) + 1
         with tf.variable_scope('cnn_1d_fuse'):
             fuse_feature =tf.concat(axis=-1,values=feature_map_list)
             print('fuse_feature.shape: ', fuse_feature.shape)
@@ -480,7 +480,7 @@ class Autoencoder:
     '''
     def cnn_2d_fuse(self, feature_map_list, is_training, suffix = '', output_dim = 1, seed=None):
         # var_scope = "2d_data_process_" + suffix
-        output_dim = (len(feature_map_list) / 4) + 1
+        output_dim = int(len(feature_map_list) / 4) + 1
         with tf.variable_scope('cnn_2d_fuse'):
             fuse_feature =tf.concat(axis=-1,values=feature_map_list)
             # Convolution Layer with 32 filters and a kernel size of 5
