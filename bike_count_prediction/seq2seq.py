a98f230203a6161301b4beb5c64e1eb81d4aabea
annieyan <annieyan9033@hotmail.com>
2020-05-08 13:55:35 -0700

    fix

--- bike_count_prediction/seq2seq.py
+++ bike_count_prediction/seq2seq.py
@@ -76,11 +76,13 @@ class generateData(object):
                     data_ = data['total_count'].iloc[i + self.timesteps:  i + self.timesteps + PREDICTION_STEPS].as_matrix()
                     rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
             elif decoder_inputs:
-                data_ = data.iloc[i: i + self.timesteps].as_matrix()
-                rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
-            else:
                 data_ = data.iloc[i + self.timesteps:  i + self.timesteps + PREDICTION_STEPS].as_matrix()
                 rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+
+            else:
+                data_ = data.iloc[i: i + self.timesteps].as_matrix()
+                rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+
         return np.array(rnn_df, dtype=np.float32)
 
 
