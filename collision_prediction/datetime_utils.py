f5541b5cb94210eab538b166b41bb2203afbd4d5
annieyan <annieyan9033@hotmail.com>
2020-05-14 13:29:37 -0700

    fix

--- collision_prediction/datetime_utils.py
+++ collision_prediction/datetime_utils.py
@@ -1,11 +1,11 @@
 import datetime
 
 def str_to_datetime(input_str):
-    datetimeFormat = "%Y-%m-%d %H:%M:%S"
+    datetimeFormat = "%Y-%m-%d"
     return datetime.datetime.strptime(input_str,datetimeFormat)
 
 def datetime_to_str(input_datetime):
-    return input_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    return input_datetime.strftime("%Y-%m-%d")
 
 
 # given a start, end time, generate a series of hour
@@ -25,11 +25,11 @@ def datetime_range(start, end, delta):
 # compute the total time in hours of period in consideration
 # e.g.: return 360 for ('betweem 2017-10-01 00:00:00' and '2017-10-15 23:59:59')
 # input: strings. eg: '2017-10-01'
-def get_total_3hour_range(start_date, end_date):
-    datetimeFormat = "%Y-%m-%d %H:%M:%S"
-    t1 = start_date+' 00:00:00'
-    t2 = end_date+' 23:59:59'
+def get_total_daily_range(start_date, end_date):
+    datetimeFormat = "%Y-%m-%d"
+    t1 = start_date
+    t2 = end_date
         #print pd.Timedelta(t1 - t2).total_seconds()
         #hour_gap = pd.Timedelta(t1 - t2).total_seconds() / 3600
     timedelta = datetime.datetime.strptime(t2, datetimeFormat) - datetime.datetime.strptime(t1,datetimeFormat)
-    return (timedelta.days + 1) * 8
+    return timedelta.days + 1
